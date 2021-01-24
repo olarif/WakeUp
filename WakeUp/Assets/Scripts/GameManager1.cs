54f709600acdf900ed28ed98979b905e7aacdde3
olarif <76481740+knusperfick@users.noreply.github.com>
2021-01-24 18:51:58 +0100

    boop

--- WakeUp/Assets/Scripts/GameManager1.cs
+++ WakeUp/Assets/Scripts/GameManager1.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class GameManager1 : MonoBehaviour
 {
+    private static GameManager1 instance;
+    public Vector2 lastCheckPointPos;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     public void EndGame()
     {
         Debug.Log("Game Over");
     }
+
+ 
 }
