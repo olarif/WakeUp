6d9c28c24b3f22fd38e4308696dd40cfab278929
mona9992 <76748531+mona9992@users.noreply.github.com>
2021-01-27 16:59:06 +0330

    FmodScripts

---
+++ WakeUp/Assets/Scripts/FmodPlayer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FmodPlayer : MonoBehaviour
+{
+    private float distance = 0.1f;
+    private float Material;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MaterialCheck();
+        
+    }
+    void MaterialCheck()
+    {
+        RaycastHit2D hit;
+        hit = Physics2D.Raycast(transform.position, Vector2.down, distance, 1 << 6);
+        if (hit.collider)
+         {
+            if (hit.collider.tag == "Floor")
+            {
+                Material = 1f;
+            }
+            else if (hit.collider.tag == "Gear")
+            {
+                Material = 2f;
+            }
+            else Material = 1f;
+         }
+    }
+
+    void PlayFootstepsEvent(string path)
+    {
+        FMOD.Studio.EventInstance Footsteps = FMODUnity.RuntimeManager.CreateInstance(path);
+        Footsteps.setParameterByName("Material", Material);
+        Footsteps.start();
+        Footsteps.release();
+    }
+}
---
+++ WakeUp/Assets/Scripts/PistonAudio.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistonAudio : MonoBehaviour
+{
+    private Vector3 pos, oldpos;
+    private FMOD.Studio.EventInstance pistonLoop;
+    private FMOD.Studio.PLAYBACK_STATE pbState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pistonLoop = FMODUnity.RuntimeManager.CreateInstance("");
+        
+        oldpos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        pistonLoop.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform, GetComponent<Rigidbody2D>()));
+        pistonLoop.getPlaybackState(out pbState);
+        pos = transform.position;
+        if(pos!=oldpos)
+        {
+            if(pbState!=FMOD.Studio.PLAYBACK_STATE.PLAYING)
+            {
+                pistonLoop.start();
+            }
+        }
+        else if(pos == oldpos&&pbState== FMOD.Studio.PLAYBACK_STATE.PLAYING)
+        {
+            pistonLoop.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        }
+        oldpos = pos;
+    }
+     void OnDestroy()
+    {
+        pistonLoop.release();
+    }
+}
