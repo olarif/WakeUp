f8e1e9ccb9fef7fb2f67c5b457cf87c9ce47a2d4
mona9992 <76748531+mona9992@users.noreply.github.com>
2021-01-30 22:20:09 +0330

    FmodFix

--- WakeUp/Assets/Scripts/FmodPlayer.cs
+++ WakeUp/Assets/Scripts/FmodPlayer.cs
@@ -6,17 +6,17 @@ public class FmodPlayer : MonoBehaviour
 {
     public Transform groundCheck;
     private float distance = 0.1f;
+    private float radius = 0.5f;
     private float Material;
     private bool wasGrounded = true;
-    //private bool isGrounded;
     RaycastHit2D hit;
 
     private float Height, oldHeight, Height_Difference;
-    FMOD.Studio.EventInstance Footsteps;
+    //FMOD.Studio.EventInstance Footsteps;
 
     private void Start()
     {
-        Footsteps = FMODUnity.RuntimeManager.CreateInstance("event:/Footsteps");
+       // Footsteps = FMODUnity.RuntimeManager.CreateInstance("event:/Footsteps");
     }
 
     // Update is called once per frame
@@ -53,38 +53,40 @@ public class FmodPlayer : MonoBehaviour
          }
     }
 
-    void PlayFootstepsEvent()
+    void PlayFootstepsEvent(string path)
     {
-       // FMOD.Studio.EventInstance Footsteps = FMODUnity.RuntimeManager.CreateInstance(path);
+        FMOD.Studio.EventInstance Footsteps = FMODUnity.RuntimeManager.CreateInstance(path);
         Footsteps.setParameterByName("Material", Material);
         Footsteps.start();
         Footsteps.release();
     }
     bool isGrounded()
     {
-       return hit = Physics2D.Raycast(groundCheck.position, Vector3.down, distance, 1 << 6);
+      // return hit = Physics2D.Raycast(groundCheck.position, Vector3.down, distance, 1 << 6);
+      return Physics2D.OverlapCircle(groundCheck.position, radius, 1 << 6);
 
     }
     void PlayerJump()
     {
-        if (!isGrounded() && wasGrounded)
+        if (!isGrounded() && wasGrounded&&Input.GetButton("Jump"))
         {
             FMOD.Studio.EventInstance Jumping = FMODUnity.RuntimeManager.CreateInstance("event:/Jumping");
             Jumping.setParameterByName("Velocity", Height_Difference);
-            Footsteps.setParameterByName("LowVolume", Height_Difference);
+            //Footsteps.setParameterByName("LowVolume", Height_Difference);
             Jumping.start();
             Jumping.release();
             Debug.Log(Height_Difference);
 
         }
+       
+
     }
     void PlayerLanded()
     {
         if(isGrounded()&&!wasGrounded)
         {
            FMOD.Studio.EventInstance Landing = FMODUnity.RuntimeManager.CreateInstance("event:/Landing");
-            Landing.setParameterByName("Velocity", Height_Difference);
-            Footsteps.setParameterByName("LowVolume", Height_Difference);
+            //Landing.setParameterByName("Velocity", Height_Difference);
             Landing.start();
             Landing.release();
             Debug.Log(Height_Difference);
